5e2dce94eb6a9a21e8edf6fc12a2924ba9966c60
13103897 <13103897@brookes.ac.uk>
2014-03-19 23:02:03 +0000

    Added Minu's work
    youpi , we 've got some css to work on

--- config/config.php
+++ config/config.php
@@ -16,7 +16,7 @@
 define("DB_HOST", "127.0.0.1");
 define("DB_NAME", "login");
 define("DB_USER", "root");
-define("DB_PASS", "mysql");
+define("DB_PASS", "");
 
 /**
  * Configuration for: Cookies
@@ -50,31 +50,25 @@ define("COOKIE_SECRET_KEY", "1gp@TMPS{+$78sfpMJFe-92s");
  * Please note that there are several issues with gmail, like gmail will block your server
  * for "spam" reasons or you'll have a daily sending limit. See the readme.md for more info.
  *
- * define("EMAIL_USE_SMTP", true);
- * define("EMAIL_SMTP_HOST", "ssl://smtp.gmail.com");
- * define("EMAIL_SMTP_AUTH", true);
- * define("EMAIL_SMTP_USERNAME", "xxxxxxxxxx@gmail.com");
- * define("EMAIL_SMTP_PASSWORD", "xxxxxxxxxxxxxxxxxxxx");
- * define("EMAIL_SMTP_PORT", 465);
- * define("EMAIL_SMTP_ENCRYPTION", "ssl");
+
  *
  * It's really recommended to use SMTP!
  *
  */
-define("EMAIL_USE_SMTP", false);
-define("EMAIL_SMTP_HOST", "yourhost");
-define("EMAIL_SMTP_AUTH", true);
-define("EMAIL_SMTP_USERNAME", "yourusername");
-define("EMAIL_SMTP_PASSWORD", "yourpassword");
-define("EMAIL_SMTP_PORT", 465);
-define("EMAIL_SMTP_ENCRYPTION", "ssl");
+ define("EMAIL_USE_SMTP", true);
+ define("EMAIL_SMTP_HOST", "ssl://smtp.gmail.com");
+ define("EMAIL_SMTP_AUTH", true);
+ define("EMAIL_SMTP_USERNAME", "meetingplannerseven@gmail.com");
+ define("EMAIL_SMTP_PASSWORD", "ROCOCOSTEINITZ");
+ define("EMAIL_SMTP_PORT", 465);
+ define("EMAIL_SMTP_ENCRYPTION", "ssl");
 
 /**
  * Configuration for: password reset email data
  * Set the absolute URL to password_reset.php, necessary for email password reset links
  */
 define("EMAIL_PASSWORDRESET_URL", "http://127.0.0.1/php-login-advanced/password_reset.php");
-define("EMAIL_PASSWORDRESET_FROM", "no-reply@example.com");
+define("EMAIL_PASSWORDRESET_FROM", "no-reply@groupesevenmail.com");
 define("EMAIL_PASSWORDRESET_FROM_NAME", "My Project");
 define("EMAIL_PASSWORDRESET_SUBJECT", "Password reset for PROJECT XY");
 define("EMAIL_PASSWORDRESET_CONTENT", "Please click on this link to reset your password:");
