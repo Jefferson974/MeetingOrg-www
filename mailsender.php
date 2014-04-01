018475a4fd9b5d6d697614b3a576eafb37f9b5ab
13103897 <13103897@brookes.ac.uk>
2014-04-02 00:52:20 +0100

    Comenting wave 1

--- mailsender.php
+++ mailsender.php
@@ -3,13 +3,14 @@
 require_once (__DIR__.'/../swift/lib/swift_required.php');
 
 
-
+//Sends email to the different guests
 function sendMails($listmail, $meeting) {
 
-
+//Connects to the SMTP server
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
   ->setUsername('meetingplannerseven@gmail.com')
   ->setPassword('ROCOCOSTEINITZ');
+
 
 $mailer = Swift_Mailer::newInstance($transport);
 //listedesinvite + objet meeting.
@@ -19,7 +20,7 @@ $message = Swift_Message::newInstance('Invitation to a university event.')
   ->setBody("Dear Student, <br> you have been invited to the event..<br><br><br><br>".$meeting->getTitle()."<br><br>Date:".$meeting->getStartDate()."<br><br><br>Place: ".$meeting->getPlace()."<br><br><br>Duration:".$meeting->getPlace() );
 
 
-
+//send the e-mails
 $result = $mailer->send($message);
 
 
